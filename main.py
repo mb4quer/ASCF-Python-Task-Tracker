cb6b6838b802447c5b29034f718147660cc49566
Mohammad <mb4quer@gmail.com>
2025-11-10 21:54:11 -0500

    Conversion complete.

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Task Tracker Login")
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
-#Window sizing and positioning
-window_width = 500
-window_height = 500
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-center_x = (screen_width // 2) - (window_width // 2)
-center_y = (screen_height // 2) - (window_height // 2)
-root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
-root.configure(bg="blue")
+Window.size = (500, 500)
+Window.clearcolor = ("blue")
 
-label = tk.Label(root, text = "Welcome, Al-Ayn Member!",
-                 font=("Calibri", 20),
-                 fg = "black",
-                 justify="center",
-                 bg = "yellow")
-label.pack(padx=50, pady=50)
-
-root.mainloop()
+class MyApp(App):
+  def build(self):
+    layout = BoxLayout()
+    layout.add_widget(Label(text="First Line\nHi Lol."))
+    return layout
+if __name__ == '__main__':
+  MyApp().run()
